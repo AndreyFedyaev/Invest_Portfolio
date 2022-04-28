fd0b7e0cc82cd2108ecb06f7fa3ce9e742bd307d
Fedyaev Andrey <Fedyaev-A-A@mail.ru>
2022-04-28 16:48:43 +0300

    изменение интерфейса

--- Invest_Portfolio/MainWindow.xaml.cs
+++ Invest_Portfolio/MainWindow.xaml.cs
@@ -20,73 +20,10 @@ namespace Invest_Portfolio
     /// </summary>
     public partial class MainWindow : Window
     {
-        SolidColorBrush Click_Background;
-        SolidColorBrush Click_Foreground;
-        SolidColorBrush Button_Background;
-        SolidColorBrush Button_Foreground;
         public MainWindow()
         {
             InitializeComponent();
-            Style_Button_Menu();
-        }
-        public void Style_Button_Menu ()
-        {
-           Click_Background = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
-           Click_Foreground = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
-
-           Button_Background = new SolidColorBrush(Color.FromArgb(255, 35, 35, 35));
-           Button_Foreground = new SolidColorBrush(Color.FromArgb(255, 155, 155, 155));
-        }
-
-        private void Button_Menu_1 (object sender, RoutedEventArgs e)
-        {
-            Menu_1.Background = Click_Background;
-            Menu_2.Background = Button_Background;
-            Menu_3.Background = Button_Background;
-            Menu_4.Background = Button_Background;
-
-            Menu_1.Foreground = Click_Foreground;
-            Menu_2.Foreground = Button_Foreground;
-            Menu_3.Foreground = Button_Foreground;
-            Menu_4.Foreground = Button_Foreground;
-        }
-        private void Button_Menu_2(object sender, RoutedEventArgs e)
-        {
-            Menu_1.Background = Button_Background;
-            Menu_2.Background = Click_Background;
-            Menu_3.Background = Button_Background;
-            Menu_4.Background = Button_Background;
-
-            Menu_1.Foreground = Button_Foreground;
-            Menu_2.Foreground = Click_Foreground;
-            Menu_3.Foreground = Button_Foreground;
-            Menu_4.Foreground = Button_Foreground;
-        }
-        private void Button_Menu_3(object sender, RoutedEventArgs e)
-        {
-            Menu_1.Background = Button_Background;
-            Menu_2.Background = Button_Background;
-            Menu_3.Background = Click_Background;
-            Menu_4.Background = Button_Background;
-
-            Menu_1.Foreground = Button_Foreground;
-            Menu_2.Foreground = Button_Foreground;
-            Menu_3.Foreground = Click_Foreground;
-            Menu_4.Foreground = Button_Foreground;
-        }
-        private void Button_Menu_4(object sender, RoutedEventArgs e)
-        {
-            Menu_1.Background = Button_Background;
-            Menu_2.Background = Button_Background;
-            Menu_3.Background = Button_Background;
-            Menu_4.Background = Click_Background;
-
-            Menu_1.Foreground = Button_Foreground;
-            Menu_2.Foreground = Button_Foreground;
-            Menu_3.Foreground = Button_Foreground;
-            Menu_4.Foreground = Click_Foreground;
+            MainWindowFrame.Content = new Login();
         }
     }
-
-
 }
